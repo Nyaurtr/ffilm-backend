e13768d3ccc3d740aa297c9deee318812d3527e7
anhminh2409 <anhminhntt@gmail.com>
2024-07-17 02:19:24 +0700

    fix

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -21,8 +21,7 @@ const createUser = async (req, res) => {
                 message: 'The password is equal to confirmPassword'
             })
         }
-        console.log('isCheckEmail', isCheckEmail);
-        const res = await UserService.createUser()
+        const response = await UserService.createUser()
         return res.status(200).json(res)
     } catch(e) {
         return res.status(404).json({
