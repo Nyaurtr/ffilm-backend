3048198add0d6d0cdab7215a0faee160540bf84d
Cảnh Katurtr <bestkatavn8@gmail.com>
2024-07-18 23:07:23 +0700

    update new module

--- src/Models/articleModel.js
+++ src/Models/articleModel.js
@@ -6,6 +6,7 @@ const articleSchema = new mongoose.Schema(
     description: { type: String, max: 500 },
     imgurl: { type: String },
     likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    dislikes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     comment: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
   },
   { timestamps: true }
